4ffca24a7e40b1b5af8847b3ef59a38790780fa5
JP Ji <baka101@gmail.com>
2015-06-16 10:55:17 -0700

    improve front end: add styling, loading indicator, and red highlight for broken links

--- client/app/app.js
+++ client/app/app.js
@@ -5,11 +5,27 @@ angular.module('linkscaping', [
 .controller('linkscapingCtrl', function ($scope, LinkChecker) {
   $scope.status = {};
   $scope.links = [];
+  $scope.isLoading = false;
 
   $scope.checkStatus = function () {
+    
+    //clear out existing data
+    $scope.isLoading = true;
+    $scope.status = {};
+    $scope.links = [];
+
+    //fetch new data
     LinkChecker.checkLink($scope.uri)
       .then(function (data) {
+        $scope.isLoading = false;
         $scope.status = data.data;
+
+        data.data.links.forEach(function (item) {
+          if (item.code !== 200) {
+            item.displayClass = 'danger';
+          }
+        });
+
         $scope.links = data.data.links;
       });
   }
